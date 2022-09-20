416f00693ffa16712c102d1a4cdef79cac37a5a1
Patrick Borgeat <patrick@borgeat.de>
2022-09-20 20:32:27 +0300

    audio source feeder and some attempts to reduce batch count

---
+++ Assets/ARquatic-Visuals/Scripts/AudioLogFeeder.cs
@@ -0,0 +1,64 @@
+using System;
+using System.Collections;
+using System.Text;
+using System.Collections.Generic;
+using UnityEngine;
+
+using ARquatic.LSystem;
+
+namespace ARquatic
+{
+    [Serializable]
+    public class JSONLogFile {
+        public JSONLogEntry[] data;
+    }
+
+    [Serializable]
+    public class JSONLogEntry {
+        public float time;
+        public string payload;
+    }
+
+
+    [RequireComponent(typeof(AudioSource))]
+    public class AudioLogFeeder : MonoBehaviour
+    {
+        public TextAsset LogFile;
+        public JSONProcessor JSONProcessor;
+
+        private AudioSource audioSource;
+
+        private int currentIndex = 0;
+        private JSONLogEntry[] entries = null;
+
+        void Awake() {
+            audioSource = GetComponent<AudioSource>();
+
+            string logString = LogFile.ToString();
+            JSONLogFile file = JsonUtility.FromJson<JSONLogFile>(logString);
+            entries = file.data;
+
+            audioSource.Play();
+        }
+
+        void Update()
+        {
+            if(!audioSource.isPlaying) return;
+            if(entries == null) return;
+            if(currentIndex >= entries.Length) return;
+            
+            float currentPlaybackTime = audioSource.timeSamples / (float) audioSource.clip.frequency;
+
+            while(currentIndex < entries.Length && entries[currentIndex].time <= currentPlaybackTime) {
+                try {
+                    JSONProcessor.Process(entries[currentIndex].payload);
+                } catch(Exception e) {
+                    Debug.Log(entries[currentIndex].payload);
+                    Debug.Log(e);
+                }
+                currentIndex++;
+            }
+        }
+    }
+}
+
