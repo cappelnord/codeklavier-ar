5644238a604f96e6816ce14d40eb777df2a73024
Patrick Borgeat <patrick@borgeat.de>
2022-09-23 15:37:19 +0300

    bump to 1.5

--- Assets/Scripts/AR/ARAppUITexts.cs
+++ Assets/Scripts/AR/ARAppUITexts.cs
@@ -4,7 +4,7 @@ namespace ARquatic.App {
 
 public static class ARAppUITexts
 {
-    public const string Version = "1.4.3";
+    public const string Version = "1.5";
 
     public static string VersionString;
     public static string DateDisplayCulture;
